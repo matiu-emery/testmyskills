a7770a0e3b2e953480896ccfac27fafa16105177
matiu-emery <your.name@example.commatiu.emery@gmail.com>
2023-08-10 18:52:53 +1200

    add more functions for buying selling fighting at cave

--- public/script.js
+++ public/script.js
@@ -1,12 +1,50 @@
 let xp = 0
 let health = 100
-let gold = 50
+let gold = 100
 let currentWeapon = 0
-let fighting
+let whawhai
 let monsterHealth = 0 // Initialize monsterHealth
-let inventory = ['stick']
+let inventory = ['Rakau ']
 
-// Define ngaWaahi before using it
+//weapon list
+const patu = [
+  {
+    ingoa: 'Rakau',
+    mana: 5
+  },
+  {
+    ingoa: 'Hoari',
+    mana: 30
+  },
+  {
+    ingoa: 'Hama kuku',
+    mana: 50
+  },
+  {
+    ingoa: 'Hoari Roa',
+    mana: 100
+  },
+]
+
+const taniwha = [
+  {
+    ingoa: 'Pirawai',
+    taumata: 2,
+    hauora: 15
+  },
+  {
+    ingoa: 'Kararehe',
+    taumata: 8,
+    hauora: 60
+  },
+  {
+    ingoa: 'Pirawai',
+    taumata: 20,
+    hauora: 300
+  },
+]
+
+//locations name etc
 const ngaWaahi = [
   {
     ingoa: 'Toene tapawha',
@@ -25,6 +63,12 @@ const ngaWaahi = [
     'button text': ['Whawhai parawai', 'Whawhai kararehe', 'Haere ki te taone tapawha'],
     'button functions': [WhawhaiPirawai, WhawhaiKararehe, HokiKiTeTaone],
     text: 'Ka tomo koe ki te ana. Ka kite koe i etahi taniwha',
+  },
+  {
+    ingoa: 'Ana',
+    'button text': ['Whawhai', 'Karohia', 'Oma'],
+    'button functions': [Whakaeke, Karohia, HokiKiTeTaone],
+    text: 'Kei te whawhai koe he taniwha',
   }
 ]
 
@@ -47,6 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
   button3.onclick = WhawhaiTarakona
 })
 
+// update
 function whakahou(waahi) {
   button1.innerText = waahi['button text'][0]
   button2.innerText = waahi['button text'][1]
@@ -59,37 +104,106 @@ function whakahou(waahi) {
   text.innerText = waahi.text
 }
 
+//back to town
 function HokiKiTeTaone() {
   whakahou(ngaWaahi[0])
 }
 
+// go store
 function HaereKiTeToa() {
   whakahou(ngaWaahi[1])
 }
 
+// go to cave
 function HaereKiTeAna() {
   whakahou(ngaWaahi[2])
 }
 
-function WhawhaiPirawai() {
-  
-}
-
-function WhawhaiKararehe() {
-  
-}
-
-function WhawhaiTarakona() {
-  console.log('Fighting dragon')
-}
-
+//Buying health 
 function HokoHauora() {
-  gold = gold - 10
-  health = health + 10
+  if(gold >= 10)
+  {
+  gold -= 10
+  health += 10
+  goldText.innerText = gold
+  healthText.innerText = health
   text.innerText = 'I hoko koe te Hauora'
+  }
+  else {
+    text.innerText = 'Kaore koe i te nui o te koura hei hoko hauora'
+  }
 }
 
+//Buying weapon 
 function HokoPatu() {
-  console.log('I hoko koe he patu')
-  text.innerText = 'I hoko koe he patu'
+  if (currentWeapon < patu.length - 1) {
+    if (gold >= 30) {
+      gold -= 30
+      currentWeapon ++
+      goldText.innerText = gold
+      let newWeapon = patu[currentWeapon].ingoa + ' '
+      text.innerText = 'I hoko koe he ' + newWeapon + '.'
+      inventory.push(newWeapon)
+      text.innerText += ' Kei roto i to rarangi ingoa kei a koe ' + inventory
+    }
+    else {
+      text.innerText = 'Kaore koe i te nui o te koura hei hoko patu'
+    }
+  }
+  else {
+    text.innerText = 'Kei a koe ke te patu tino kaha.'
+    button2.innerText = 'Hokona  atu he patu mo te 15 koura'
+    button2.onclick = HokonaAtuToPatu
+  }
+}
+
+// sell weapon
+function HokonaAtuToPatu() {
+  if (inventory.length > 1 ) {
+    gold += 15
+    goldText.innerText = gold
+    let currentWeapon = inventory.shift()
+    text.innerText = 'I hoko atu koe te  ' + currentWeapon + '.'
+    text.innerText = ' Kei roto i to rarangi ingoa kei a koe ' + inventory
+  }
+  else {
+    text.innerText = 'Kaua e hoko atu to patu anake '
+  }
+}
+
+//fight slime 
+function WhawhaiPirawai() {
+  whawhai = 0
+  HaereWhawhai()
+}
+
+//fight beast 
+function WhawhaiKararehe() {
+  whawhai = 1
+  HaereWhawhai()
+}
+
+//fight dragon 
+function WhawhaiTarakona() {
+  whawhai = 2
+  HaereWhawhai()
+}
+
+// go fight
+function HaereWhawhai() {
+  whakahou(ngaWaahi[3])
+  monsterHealth = taniwha[whawhai].hauora
+  monsterStats.style.display = 'block'
+  monsterNameText.innerText = taniwha[whawhai].ingoa
+  monsterHealthText.innerText = monsterHealth
+}
+
+//Attack
+function Whakaeke() {
+  
+}
+
+//Dodge
+function Karohia() {
+  
 }
